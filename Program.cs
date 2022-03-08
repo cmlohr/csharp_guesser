e4352ca241668350908edce2ce9a66e86fe048ab
cmloh <cmloh@LAPTOP-TDJJHOC4>
2022-03-08 16:35:08 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jane Doe";
+            int age = 35;
+            // Console.WriteLine("hello " + name + ", I am " + age + " years old");
+            Console.WriteLine("(0) is (1)", name, age);
+        }
+    }
+}
